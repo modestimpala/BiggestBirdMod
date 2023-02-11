5894ff907b3b3984e203a014140a297f2365d4af
Moddy <45338507+modestimpala@users.noreply.github.com>
2023-02-10 22:58:27 -0800

    Update BirdPatch.cs

--- Patches/BirdPatch.cs
+++ Patches/BirdPatch.cs
@@ -1,10 +1,4 @@
-﻿using HarmonyLib;
-using MonoMod.RuntimeDetour;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using HarmonyLib;
 using UnityEngine;
 
 namespace BiggestBirdMod.Patches
